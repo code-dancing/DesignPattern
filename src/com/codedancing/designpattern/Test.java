3dfc4ff4133baf96f8a0fbaf5959fbc22b3384b6
lei jia <jialeichn@vip.qq.com>
2019-11-21 18:57:33 +0800

    modify

--- src/com/codedancing/designpattern/Test.java
+++ src/com/codedancing/designpattern/Test.java
@@ -1,11 +1,16 @@
 package com.codedancing.designpattern;
 
 import com.codedancing.designpattern.creational.singleton.Singleton02;
+import com.codedancing.designpattern.creational.singleton.Singleton06;
 
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(Singleton02.INSTANCE);
+        System.out.println(Singleton06.getInstance());
+        System.out.println(Singleton06.getInstance());
+        System.out.println(Singleton06.getInstance());
+        System.out.println(Singleton06.getInstance());
+        System.out.println(Singleton06.getInstance());
     }
 
 }
